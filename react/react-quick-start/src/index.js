e37a660dd28ab5529f0aaebb2534a047a24f84db
Евгений Шилов <shilovevgeniy@inbox.ru>
2026-02-19 21:23:53 +0500

    старт

---
+++ react/react-quick-start/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <><h1>Заголовок первого уровня</h1>
+    <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, dolorum?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, numquam?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, numquam?</li>
+    </ul>
+  </>
+);
