7569ec0a5b057eabfabcc0e51875f9cab27e93c3
Евгений Шилов <shilovevgeniy@inbox.ru>
2026-02-20 18:06:07 +0500

    Добавил карточки

--- react/react-quick-start/src/index.js
+++ react/react-quick-start/src/index.js
@@ -2,21 +2,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const name = "Евгений";
-const lastname = "Шилов"
-const year = new Date().getFullYear();
+const value = "Карточки";
+
+
 root.render(
-  <>
-    <h1>Заголовок первого уровня</h1>
-    <h2>{name} {lastname}</h2>
-    <ul>
-      <li>Элемент 1</li>
-      <li>Элемент 2</li>
-      <li>Элемент 3</li>
-      <li>Элемент 4</li>
-      <li>Элемент 5</li>
-      <li>Элемент 6</li>
-    </ul>
-    <p>&copy; Авторские права чьи-то {year}</p>
-  </>
+  <div className="container col-10">
+    <h1 className='h1-style m-3'>{value}</h1>
+    {Array.from({ length: 5 }, (_, index) =>
+      <div className="card m-3">
+        <img src={`https://picsum.photos/${200 + index}`} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        </div>
+      </div>)}
+    <p className='m-3'>&copy; Авторские права чьи-то {new Date().getFullYear()}</p>
+  </div>
 );
