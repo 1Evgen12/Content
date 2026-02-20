408748a272ae934c629236f1b8b7b3f685933d3e
Евгений Шилов <shilovevgeniy@inbox.ru>
2026-02-20 10:14:14 +0500

    react переменные

--- react/react-quick-start/src/index.js
+++ react/react-quick-start/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = "Евгений";
+const lastname = "Шилов"
+const year = new Date().getFullYear();
 root.render(
-  <><h1>Заголовок первого уровня</h1>
+  <>
+    <h1>Заголовок первого уровня</h1>
+    <h2>{name} {lastname}</h2>
     <ul>
-      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, dolorum?</li>
-      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, numquam?</li>
-      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, numquam?</li>
+      <li>Элемент 1</li>
+      <li>Элемент 2</li>
+      <li>Элемент 3</li>
+      <li>Элемент 4</li>
+      <li>Элемент 5</li>
+      <li>Элемент 6</li>
     </ul>
+    <p>&copy; Авторские права чьи-то {year}</p>
   </>
 );
