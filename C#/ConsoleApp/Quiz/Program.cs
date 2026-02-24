c4e71c82c35f40a3bebe08469b39d307bc387c1f
Евгений Шилов <shilovevgeniy@inbox.ru>
2026-02-24 14:58:30 +0500

    Добавил цикл

--- C#/ConsoleApp/Quiz/Program.cs
+++ C#/ConsoleApp/Quiz/Program.cs
@@ -8,21 +8,29 @@ namespace Quiz
         {    
 
             int score = 0;
-            string answer1 = "6", answer2 = "жир", answer3 = "звезда", answer4 = "4 года";
 
-            Console.WriteLine("Сколько будет 2 + 2 * 2? [8, 6, 4, 2]");
-            string question1 = Console.ReadLine();
-            Console.WriteLine("Что у верблюда в горбу? [вода, мясо, жир, кость]");
-            string question2 = Console.ReadLine();
-            Console.WriteLine("Солнце - это ... [планета, rомета, астероид, звезда]");
-            string question3 = Console.ReadLine();
-            Console.WriteLine("Високосный год бывает раз в ... [4 года, 1 год, 3 года, 5 лет]");
-            string question4 = Console.ReadLine();
+            string[] answers = ["6", "жир", "звезда", "4 года"];
+            string[] questions = ["Сколько будет 2 + 2 * 2? [8, 6, 4, 2]",
+                "Что у верблюда в горбу? [вода, мясо, жир, кость]",
+                "Солнце - это ... [планета, комета, астероид, звезда]",
+                "\"Високосный год бывает раз в ... [4 года, 1 год, 3 года, 5 лет]"
+                ];
 
-            if (question1.ToLower() == answer1) score += 10;
-            if (question2.ToLower() == answer2) score += 10;
-            if (question3.ToLower() == answer3) score += 10;
-            if (question4.ToLower() == answer4) score += 10;
+            for (int i = 0; i < questions.Length; i++)
+            {
+                string question = questions[i];
+                Console.WriteLine(question);
+                string answer = Console.ReadLine().ToLower().Trim();
+                if (answers[i] == answer)
+                {
+                    Console.WriteLine("Все верно!");
+                    score += 10;
+                }
+                else
+                {
+                    Console.WriteLine("Пока не правильно");
+                }
+            }
             Console.WriteLine("\n----------------------");
             Console.WriteLine($"Вы набрали {score} баллов");
 
--- C#/ConsoleApp/Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ C#/ConsoleApp/Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3be85b435338131935fa79416e67a75f008175b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e6d0958a09fc0157dfc4cc47af9775cfaf47b5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
